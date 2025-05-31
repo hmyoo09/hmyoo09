f0f9c6633f33b429f5ff0886aa87c5e1e9a6310b
hmyoo09 <hmforyou0121@gmail.com>
2025-05-31 14:37:52 +0900

    Update inoutput(UI).py

--- inoutput(UI).py
+++ inoutput(UI).py
@@ -2,13 +2,33 @@ import gradio as gr
 import os
 import json
 
-def file_upload_maketxt(file):
+def find_empty_space(text_path):
+    ToDoWords = ['#code:', '설명', '아래에 코드 작성']
+    ToDoList = []
+
+    try:
+        with open(text_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+    except FileNotFoundError:
+        return "text파일이 정상적으로 만들어지지 않았습니다."
+
+    for line in lines:
+        if any(word in line for word in ToDoWords):
+            ToDoList.append(line.strip()+'=>'+f' {1}번째 Cell')
+
+    if len(ToDoList)!=0:
+        return '\n'.join([f"{i+1}. {ToDoList[i]}" for i in range(len(ToDoList))])
+    else:
+        return '고칠 부분이 없습니다.'
+
+def show_UI(file):
     if file is None:
         raise gr.Error("파일이 존재하지 않습니다.")
-    file_path=file.name
-    file_size=os.path.getsize(file_path)
     
-    with open(file.name, 'r', encoding='utf-8') as f:
+    file_path = file.name
+    file_size = os.path.getsize(file_path)
+
+    with open(file_path, 'r', encoding='utf-8') as f:
         notebook_data = json.load(f)
 
     json_as_text = json.dumps(notebook_data, indent=2, ensure_ascii=False)
@@ -19,18 +39,38 @@ def file_upload_maketxt(file):
     with open(output_path, 'w', encoding='utf-8') as f:
         f.write(json_as_text)
 
-    return f"파일명: {file_path}\n크기: {file_size} bytes\n\n" \
-           f"tt.txt가 바탕화면에 저장되었습니다:\n{output_path}"
+    todo_text = find_empty_space(output_path)
 
+    info_str = f"파일명: {file_path}\n크기: {file_size} bytes\n\n"+f"tt.txt가 바탕화면에 저장되었습니다:\n{output_path}"
 
+    return info_str, todo_text
 
-iface = gr.Interface(
-    fn=file_upload_maketxt,
-    inputs=gr.File(label="ipynb 파일 업로드",file_types=['.ipynb']),
-    outputs="text",
-    title="코랩 암살 피하기!",
-    description="ipynb 파일을 업로드하면 바탕화면에 tt.txt로 저장해드립니다.",
-    allow_flagging="never",
-)
+def main():
+    # 기존 tt.txt 내용 초기화
+    try:
+        desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+        text_path = os.path.join(desktop_path, 'tt.txt')
+        with open(text_path, 'w', encoding='utf-8') as f:
+            f.write('')
+    except:
+        pass
+    
+        
+    with gr.Blocks() as iface:
+        desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+        text_path = os.path.join(desktop_path, 'tt.txt')
+        gr.Markdown("# 코랩 암살 피하기!")
 
-iface.launch(share=True)
+        file_input = gr.File(label="ipynb 파일 업로드", file_types=['.ipynb'])
+        output_text = gr.Textbox(label='파일 경로와 크기', interactive=False)
+        finding = gr.Textbox(label='고칠 부분', interactive=False)
+
+        file_input.change(
+            fn=show_UI,
+            inputs=file_input,
+            outputs=[output_text, finding]
+        )
+
+    iface.launch()
+
+main()
