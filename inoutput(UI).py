e0202cff5a4f32bafd9503918514198172eb4f5d
hmyoo09 <hmforyou0121@gmail.com>
2025-06-09 01:10:15 +0900

    Update inoutput(UI).py

--- inoutput(UI).py
+++ inoutput(UI).py
@@ -9,11 +9,27 @@ import nbformat
 from nbclient import NotebookClient
 from nbclient.exceptions import CellExecutionError
 import ast
-import playsound
-
+from playsound import playsound
 import sys
 import importlib.util
 import os
+import time
+import pyautogui
+from datetime import datetime
+
+def capture_screen_to_file():
+    try:
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        screenshot_path =f"./gradio_live_capture_{timestamp}.png"
+
+        screenshot = pyautogui.screenshot()
+        screenshot.save(screenshot_path)
+
+        print(f"화면 캡처 완료: {screenshot_path}")
+        return f"화면 캡처 완료! 저장 위치: {screenshot_path}"
+    except Exception as e:
+        print(f"[캡처 실패] {e}")
+        return f"캡처 실패: {e}"
 
 def is_stdlib_module(module_name):
     try:
@@ -47,14 +63,20 @@ def is_pypi_module(module_name):
         # PyPI 설치 경로 내부에 있는지 확인
         return any(module_path.startswith(os.path.abspath(site_path)) for site_path in site_paths)
     except Exception as e:
-        print(f"⚠️ 확인 실패: {e}")
         return False
+
+def play_start_sound():
+    try:
+        playsound("efan.wav")  # 🎵 시작 알림 사운드
+    except Exception as e:
+        print(f"알림음 재생 실패: {e}")
+
 
 def sound():
     try:
         playsound("beep-03.wav")
     except Exception as audio_err:
-        print(f"🔈 소리 재생 실패: {audio_err}")
+        print(f"소리 재생 실패: {audio_err}")
 
 def check_module(string):
     not_available=['google','%','%%']
@@ -74,25 +96,36 @@ def extract_imports(source_code: str):
     try:
         tree = ast.parse(source_code)
     except SyntaxError:
-        return ""
+        return "", []
 
     lines = source_code.splitlines()
     import_lines = []
+    un_imports = []
 
     for node in ast.walk(tree):
         if isinstance(node, ast.Import):
             for alias in node.names:
                 modname = alias.name.split('.')[0]
+                asname = alias.asname or alias.name
+                asname=asname if asname else ''
                 if is_pypi_module(modname) or is_stdlib_module(modname):
                     import_lines.append(lines[node.lineno - 1].strip())
+                else:
+                    un_imports.append((alias.name, asname))
 
         elif isinstance(node, ast.ImportFrom):
             if node.module:
                 modname = node.module.split('.')[0]
+                for alias in node.names:
+                    full_name = f"{node.module}.{alias.name}"
+                    asname = alias.asname or alias.name
+                    asname=asname if asname else ''
                 if is_pypi_module(modname) or is_stdlib_module(modname):
                     import_lines.append(lines[node.lineno - 1].strip())
+                else:
+                    un_imports.append((full_name, asname))
 
-    return '\n'.join(import_lines)
+    return '\n'.join(import_lines), un_imports
 
 def extract_functions(source_code:str):
     func_dict = {}
@@ -134,7 +167,6 @@ def extract_variable_definitions(source_code: str):
     var_defs = {}
 
     for node in ast.walk(tree):
-        # 1. 변수 정의 (a = 1, a, b = ...)
         if isinstance(node, ast.Assign):
             line = lines[node.lineno - 1].strip()
             targets = node.targets
@@ -190,10 +222,12 @@ def find_empty_space(text_path):
 def find_error(ipynb_path):
     just_errors=''
     errors_but_ok=''
+    module_not_found=[]
     ok_to_make_error=False
     needed_libs=''
     needed_funcs={}
     needed_varis={}
+    need_inputs=''
     with open(ipynb_path, 'r', encoding='utf-8') as f:
         nb = nbformat.read(f, as_version=4)
 
@@ -205,6 +239,10 @@ def find_error(ipynb_path):
             continue
         
         if not check_module(cell['source']):
+            continue
+        
+        if 'input' in cell['source']:
+            need_inputs+='\n'+f'Cell {i+1} input이 필요함'
             continue
 
         try:
@@ -225,12 +263,28 @@ def find_error(ipynb_path):
             needed_varis.update(in_cell_varis)
         except: pass
 
-        try:
-            needed_libs+='\n'+extract_imports(cell['source'])
-        except TypeError:
-            needed_libs+='\n'+extract_imports('\n'.join(cell['source']))
+        result = extract_imports(cell['source'])
+        add_libs, un_imports = result if isinstance(result, tuple) and len(result) == 2 else ("", [])
+        needed_libs+='\n'+add_libs
+        module_not_found+=un_imports
 
-        
+        tmp=False
+        for names in module_not_found:
+            if names[0] in cell['source']:
+                idx=cell['source'].find(names[0])
+                if cell['source'][idx+len(names[0])]=='.':
+                    tmp=True
+                    break
+            if names[1] in cell['source']:
+                idx=cell['source'].find(names[1])
+                if cell['source'][idx+len(names[1])]=='.':
+                    tmp=True
+                    break
+
+        if tmp:
+            continue
+
+
         tmp=False
         for line in cell['source'].split('\n'):
             if '#' in line and 'Error' in line:
@@ -242,12 +296,14 @@ def find_error(ipynb_path):
         
         for func_name,func_content in needed_funcs.items():
             if func_name in in_cell_funcs:
-                if func_content==in_cell_funcs[func_name]:
-                    continue
+                continue
             else:
                 if func_name in cell['source']:
+                    cell['source']=''.join(concat_sources(func_content,cell['source']))
+                    '''
                     if cell['source'][cell['source'].find(func_name)+len(func_name)]=='(':
                         cell['source']=''.join(concat_sources(func_content,cell['source']))
+                    '''
 
         for vari_name,vari_content in needed_varis.items():
             if vari_name in in_cell_varis:
@@ -256,25 +312,31 @@ def find_error(ipynb_path):
                 cell['source']=''.join(concat_sources(vari_content,cell['source']))
 
         cell['source']=''.join(concat_sources(needed_libs,cell['source']))
-        print(cell['source'])
+        #print(cell['source'])
 
         single_cell_nb = nbformat.v4.new_notebook(cells=[cell])
         try:
-            print(f"\n📦 Cell {i+1} 실행 중...")
+            #print(f"\Cell {i+1} 실행 중...")
             client = NotebookClient(single_cell_nb, timeout=20, kernel_name='python3')
             client.execute()
-            print(f"✅ Cell {i+1} 성공")
+            #print(f"Cell {i+1} 성공")
         except CellExecutionError as e:
-            print(f"❌ Cell {i+1} 실패: {e.ename} - {e.evalue}")
+            #print(f"Cell {i+1} 실패: {e.ename} - {e.evalue}")
             if not ok_to_make_error:
                 just_errors+='\n'+f'Cell {i+1} 실패: {e.ename} - {e.evalue}'
             else:
                 errors_but_ok+='\n'+f'Cell {i+1} 에러명 이거 맞지?: {e.ename} - {e.evalue}'
                 ok_to_make_error=False
 
-    print(needed_funcs)
+    if module_not_found:
+        mo='\n'.join([f'모듈:{tup[0]} 별칭:{tup[1]}' for tup in module_not_found])
+    else:
+        mo='해당사항 없음'
     
-    return just_errors, errors_but_ok
+    #print(in_cell_funcs)
+    #print(needed_funcs)
+
+    return just_errors, errors_but_ok, mo, need_inputs
     
 def show_UI(file):
     if file is None:
@@ -295,13 +357,12 @@ def show_UI(file):
     with open(output_path, 'w', encoding='utf-8') as f:
         f.write(json_as_text)
 
-    errors,errors_but_ok=find_error(file_path)
+    errors,errors_but_ok,module_errors,need_inputs=find_error(file_path)
 
     todo_text = find_empty_space(output_path)
 
     info_str = f"파일명: {file_path}\n크기: {file_size} bytes\n\n"+f"tt.txt가 바탕화면에 저장되었습니다:\n{output_path}"
-
-    return info_str, todo_text, errors, errors_but_ok
+    return info_str, todo_text, errors, errors_but_ok ,module_errors,need_inputs
 
 def main():
     # 기존 tt.txt 내용 초기화
@@ -312,9 +373,8 @@ def main():
             f.write('')
     except:
         pass
-    
-        
-    with gr.Blocks() as iface:
+     
+    with gr.Blocks() as iface:  
         desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
         text_path = os.path.join(desktop_path, 'tt.txt')
         gr.Markdown("# 코랩 암살 피하기!")
@@ -324,13 +384,21 @@ def main():
         finding = gr.Textbox(label='고칠 부분', interactive=False)
         errors=gr.Textbox(label='에러난 부분',interactive=False)
         errors_but_ok=gr.Textbox(label='에러나도 되는 부분',interactive=False)
+        module_errors=gr.Textbox(label='모듈을 못잧겠음',interactive=False)
+        need_inputs=gr.Textbox(label='이건 직접 입력해서 테스트 ㄱㄱ', interactive=False)
 
         file_input.change(
             fn=show_UI,
             inputs=file_input,
-            outputs=[output_text, finding, errors, errors_but_ok]
+            outputs=[output_text, finding, errors, errors_but_ok,module_errors,need_inputs]
         )
+        capture_btn = gr.Button("현재 브라우저 화면 캡처하기")
+        capture_result = gr.Textbox(label="캡처 결과", interactive=False)
 
-    iface.launch()
+        capture_btn.click(fn=capture_screen_to_file, outputs=capture_result)
+        play_start_sound() 
 
-main()
+    iface.launch(server_name="127.0.0.1", server_port=7860)
+
+if __name__=='__main__':
+    main()
